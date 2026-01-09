1fa44523aac5a1a6090e8904d46c4cadafe7f0ff
samueladole <adoleichasamuel@gmail.com>
2026-01-09 14:26:21 +0000

    Refactor: Enhance old message reading and email alert functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """Main module to monitor Telegram channel for job postings and send email alerts."""
+
 import os
 from telethon import TelegramClient, events
 from dotenv import load_dotenv
-from filters import is_relevant, contains_remote_option
+from filters import get_offset_date, is_relevant, contains_remote_option
 from emailer import send_email
 
 load_dotenv()
@@ -12,6 +13,52 @@ api_hash = os.getenv("API_HASH")
 channel = os.getenv("TARGET_CHANNEL")
 
 client = TelegramClient("session", api_id, api_hash)
+
+
+# Read old messages and apply filters
+async def read_old_messages(client: TelegramClient, channel: str):
+    """Reads old messages from the target channel and applies filters."""
+    print("📨 Reading old messages...")
+    offset_date = get_offset_date(days=2)
+    async for message in client.iter_messages(
+        channel,
+        offset_date=offset_date,
+        limit=100,
+        reverse=True
+    ):
+        if message.date < offset_date:
+            continue
+        print("🔍 Checking old message...")
+        text = message.text
+        if not text:
+            continue
+
+        if is_relevant(text) and contains_remote_option(text):
+            print("✅ MATCH FOUND (Old Message)")
+            link = None
+            if message.entities:
+                for entity in message.entities:
+                    if hasattr(entity, "url"):
+                        link = entity.url
+
+            email_body = f"""
+            Old Job Alert 🚀
+
+            {text}
+
+            Link: {link if link else "No link found"}
+            """
+
+            send_email(
+                subject="New Developer / Data Job Found (Old Message)",
+                content=email_body,
+            )
+
+            print("📧 Email sent for old message")
+        else:
+            print("❌ Old job posted but no match")
+    print("📨 Finished reading old messages.")
+
 
 @client.on(events.NewMessage(chats=channel))
 async def handler(event):
@@ -36,24 +83,25 @@ async def handler(event):
 
             {text}
 
-            Link: {link if link else 'No link found'}
+            Link: {link if link else "No link found"}
             """
 
-            send_email(
-            subject="New Developer / Data Job Found",
-            content=email_body
-            )
+            send_email(subject="New Developer / Data Job Found", content=email_body)
 
             print("📧 Email sent")
     else:
         print("❌ Job posted but no match")
 
+
 async def main():
     """Main function to start the Telegram client."""
     await client.start()
     print("🤖 Listening to channel...")
+    await read_old_messages(client, channel)
     await client.run_until_disconnected()
+
 
 if __name__ == "__main__":
     import asyncio
+
     asyncio.run(main())
